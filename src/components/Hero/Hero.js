4931121278a286208ff041637ea4b285f6ffb5e1
Yusuf <yusufmohd72@gmail.com>
2021-10-23 20:48:09 +0530

    Finally accessed env in component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+// import { useEffect } from 'react';
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const Hero = () => {
+  console.log(apiKey);
+  //   console.log(API_KEY);
+  //   console.log(API_KEY);
+  //   const baseUrl = `https://api.themoviedb.org/3/`;
+  //   const req = `${baseUrl}trending/movie/week?api_key=${apiKey}`;
+
+  //   fetch(req)
+  // .then(res => res.json())
+  // .then(data => console.log(data));
+
+  return <div className="hero"> a </div>;
+};
+export default Hero;
