0b0d2cca63706f7b451168570f6266492c7ef39b
Yusuf <yusufmohd72@gmail.com>
2021-10-23 18:05:33 +0530

    Init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import { Container } from './Header.styles';
+
+const Header = () => {
+  return (
+    <Container>
+      <h3>
+        <span>RMDb</span>&nbsp;&nbsp;The React Movie Database
+      </h3>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 1rem;
+  background-color: #121212;
+  color: white;
+
+  span {
+    padding: 0.3rem;
+    background-color: yellow;
+    color: black;
+    border-radius: 7px;
+  }
+`;
