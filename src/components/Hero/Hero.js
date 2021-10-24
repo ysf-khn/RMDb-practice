a9ffffcfb0b3bd6cb256dc94b422b7bdf6a398c3
Yusuf <yusufmohd72@gmail.com>
2021-10-24 20:25:26 +0530

    Hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,33 @@
-// import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { Wrapper } from './Hero.styles';
+
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const Hero = () => {
-  console.log(apiKey);
-  //   console.log(API_KEY);
-  //   console.log(API_KEY);
-  //   const baseUrl = `https://api.themoviedb.org/3/`;
-  //   const req = `${baseUrl}trending/movie/week?api_key=${apiKey}`;
+  const [path, setPath] = useState('');
 
-  //   fetch(req)
-  // .then(res => res.json())
-  // .then(data => console.log(data));
+  const baseUrl = `https://api.themoviedb.org/3/`;
+  const baseImageUrl = `https://image.tmdb.org/t/p/w1280`;
+  const req = `${baseUrl}trending/movie/week?api_key=${apiKey}`;
+  const img = `${baseUrl}configuration?api_key=${apiKey}`;
 
-  return <div className="hero"> a </div>;
+  fetch(img)
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+  fetch(req)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      console.log(data.results[0]);
+      setPath(`${baseImageUrl}${data.results[0].poster_path}`);
+    });
+
+  return (
+    <Wrapper>
+      <img src={path} alt="" />
+    </Wrapper>
+  );
 };
 export default Hero;
---
+++ src/components/Hero/Hero.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  object-fit: cover;
+  display: grid;
+  place-items: center;
+  background-color: #121212;
+
+  img {
+    height: 90vh;
+    width: auto;
+  }
+`;
