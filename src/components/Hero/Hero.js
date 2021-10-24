468cd0890c5ed9604f206ff7e8373baa3e4992fb
Yusuf <yusufmohd72@gmail.com>
2021-10-24 20:42:42 +0530

    Hero complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,6 +6,10 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 const Hero = () => {
   const [path, setPath] = useState('');
+  const [title, setTitle] = useState('');
+  const [overview, setOverview] = useState('');
+  const [rating, setRating] = useState('');
+  const [date, setDate] = useState('');
 
   const baseUrl = `https://api.themoviedb.org/3/`;
   const baseImageUrl = `https://image.tmdb.org/t/p/w1280`;
@@ -22,11 +26,21 @@ const Hero = () => {
       console.log(data);
       console.log(data.results[0]);
       setPath(`${baseImageUrl}${data.results[0].poster_path}`);
+      setTitle(`${data.results[0].original_title}`);
+      setOverview(`${data.results[0].overview}`);
+      setRating(`${data.results[0].vote_average}`);
+      setDate(`${data.results[0].release_date}`);
     });
 
   return (
     <Wrapper>
       <img src={path} alt="" />
+      <div className="details">
+        <h1 className="title">{title}</h1>
+        <p className="overview">{overview}</p>
+        <h3>Release Date: {date}</h3>
+        <h3>{rating} 🌟</h3>
+      </div>
     </Wrapper>
   );
 };
--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -1,13 +1,26 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  padding: 1rem;
   object-fit: cover;
   display: grid;
+  grid-template-columns: 1fr 1fr;
   place-items: center;
+  column-gap: 1.5rem;
   background-color: #121212;
+  color: #f3f3f3;
 
   img {
-    height: 90vh;
+    height: 85vh;
     width: auto;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  h3,
+  p {
+    margin-bottom: 1rem;
   }
 `;
